383569e7be394d070706ceb3a2fa71f1a852102c
fedy14 <jeanpierrefedy@gmail.com>
2024-12-13 19:42:11 -0400

    declare an update function

--- script.js
+++ script.js
@@ -72,3 +72,13 @@ const weapons = [
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+function update(location) {
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerText = location.text;
+}
