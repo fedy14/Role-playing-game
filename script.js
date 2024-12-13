f9d02e2df196365403060ef703a65428da7513f4
fedy14 <jeanpierrefedy@gmail.com>
2024-12-13 19:50:55 -0400

    add else into the function weapon

--- script.js
+++ script.js
@@ -116,4 +116,12 @@ function buyWeapon() {
       text.innerText = "You now have a " + newWeapon + ".";
       inventory.push(newWeapon);
       text.innerText += " In your inventory you have: " + inventory;
+    }else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
     }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }
+}
