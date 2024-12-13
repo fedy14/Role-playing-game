ceb0750f2141d2d209eafeb191cf25b1ed0c1d75
fedy14 <jeanpierrefedy@gmail.com>
2024-12-13 19:49:44 -0400

    add funtion weapon

--- script.js
+++ script.js
@@ -105,3 +105,15 @@ function buyHealth() {
     text.innerText = "You do not have enough gold to buy health.";
   }
 }
+
+function buyWeapon() {
+  if (currentWeaponIndex < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeaponIndex++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeaponIndex].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    }
