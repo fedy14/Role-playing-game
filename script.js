69d44a9362d9af56492e52b59bbb8f045d6b6970
fedy14 <jeanpierrefedy@gmail.com>
2024-12-13 19:48:15 -0400

    add function buyhealth with the statement

--- script.js
+++ script.js
@@ -94,3 +94,14 @@ function goStore() {
 function goCave() {
   update(locations[2]);
 }
+
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
