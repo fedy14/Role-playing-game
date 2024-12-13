ede925419a41d9de297cc4fc6634c5e06bb2d518
fedy14 <jeanpierrefedy@gmail.com>
2024-12-13 19:33:04 -0400

    add 3 button onclick

--- script.js
+++ script.js
@@ -67,3 +67,8 @@ const weapons = [
       text: "You are fighting a monster."
     }
   ];
+
+  // initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
